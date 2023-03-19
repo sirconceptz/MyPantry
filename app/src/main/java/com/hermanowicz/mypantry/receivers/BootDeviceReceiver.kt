99b8d02f117351d07abcabb3243907a3bed1df02
sirconceptz <mateusz.hermanowicz@mypantry.eu>
2023-03-19 17:48:31 +0100

    Implementation empty boot device receiver

---
+++ app/src/main/java/com/hermanowicz/mypantry/receivers/BootDeviceReceiver.kt
@@ -0,0 +1,13 @@
+package com.hermanowicz.mypantry.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class BootDeviceReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+
+    }
+}
