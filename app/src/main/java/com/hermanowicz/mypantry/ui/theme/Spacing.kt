b904f5e70c9aa7df4663e1807041e380995c3553
sirconceptz <mateusz.hermanowicz@mypantry.eu>
2023-03-20 15:36:06 +0100

    Add local spacing to theme & upgrade gradle

---
+++ app/src/main/java/com/hermanowicz/mypantry/ui/theme/Spacing.kt
@@ -0,0 +1,13 @@
+package com.hermanowicz.mypantry.ui.theme
+
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class Spacing(
+    val line: Dp = 1.dp,
+    val small: Dp = 8.dp,
+    val medium: Dp = 16.dp
+)
+
+val LocalSpacing = compositionLocalOf { Spacing() }
--- app/src/main/java/com/hermanowicz/mypantry/ui/theme/Theme.kt
+++ app/src/main/java/com/hermanowicz/mypantry/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -35,6 +36,8 @@ fun MyPantryTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composab
         LightColorPalette
     }
 
+    CompositionLocalProvider(LocalSpacing provides Spacing()) {
+    }
     MaterialTheme(
         colors = colors,
         typography = Typography,
